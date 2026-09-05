e012c46c14d8ec770dda8ccebce20c7e5eb7cc5e
ashik-info <kaziashik999@gmail.com>
2026-09-05 22:01:39 +0600

    Update lazy-lock.json and refactor colorscheme configuration; switch to catppuccin theme and adjust performance settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,85 @@
 return {
   {
-    "tiagovla/tokyodark.nvim",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     opts = {
-      transparent_background = false,
-      gamma = 1.00,
-
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        identifiers = { italic = true },
-        functions = {},
-        variables = {},
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = {
+        light = "latte",
+        dark = "mocha",
       },
-
-      terminal_colors = true,
+      transparent_background = false,
+      term_colors = true,
+      integrations = {
+        blink_cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = {
+          enabled = true,
+          scope_color = "lavender",
+          colored_indent_levels = false,
+        },
+        lazy = true,
+        mason = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        snacks = true,
+        telescope = {
+          enabled = true,
+        },
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
     },
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyodark",
+      colorscheme = "catppuccin",
     },
   },
 }
+-- return {
+--   {
+--     "tiagovla/tokyodark.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {
+--       transparent_background = false,
+--       gamma = 1.00,
+--
+--       styles = {
+--         comments = { italic = true },
+--         keywords = { italic = true },
+--         identifiers = { italic = true },
+--         functions = {},
+--         variables = {},
+--       },
+--
+--       terminal_colors = true,
+--     },
+--   },
+--
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "tokyodark",
+--     },
+--   },
+-- }
